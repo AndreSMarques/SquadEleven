eef272623299ae4a55333234416a49da7aa06b7c
Felipe Fracasso <93887208+FelipeM-F@users.noreply.github.com>
2024-07-17 16:56:03 -0400

    criada branch develop

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,7 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,jsx}",
-  ],
+export default {
+  content: [    "./index.html",
+    "./src/**/*.{js,jsx}",],
   theme: {
     extend: {},
   },
